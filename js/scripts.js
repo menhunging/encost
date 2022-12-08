3f4a04bff68ad864e6dd930f74bf18635ac83da6
menhunging <menhunging@gmail.com>
2022-12-08 19:16:56 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,174 @@
+$(document).ready(function () {
+
+    if ($('.tarisfList').length > 0) {
+        $('.tarisfList').slick({
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true,
+            responsive: [
+                {
+                    breakpoint: 3000,
+                    settings: "unslick"
+                },
+                {
+                    breakpoint: 1220,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 640,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                },
+            ]
+        });
+    }
+
+    if ($('.solutionsList').length > 0) {
+        $('.solutionsList').slick({
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true,
+            responsive: [
+                {
+                    breakpoint: 3000,
+                    settings: "unslick"
+                },
+                {
+                    breakpoint: 1220,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                },
+            ]
+        });
+    }
+
+    if ($('.advList').length > 0) {
+        $('.advList').slick({
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            dots: true,
+            adaptiveHeight: true,
+            responsive: [
+                {
+                    breakpoint: 3000,
+                    settings: "unslick"
+                },
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                },
+            ]
+        });
+    }
+
+    if ($('.reviewsList').length > 0) {
+        $('.reviewsList').slick({
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+            dots: false,
+            responsive: [
+
+                {
+                    breakpoint: 1220,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        arrows: false,
+                        dots: true,
+                    }
+                },
+
+            ]
+        });
+    }
+
+    if ($('.howAnimation').length > 0) {
+        let scrollOff = false
+        var block_show = null;
+        let animationAll = $('.animation')
+
+        if ($(window).width() > 768) {
+            scrollTracking();
+        }
+
+        function scrollTracking() {
+
+
+            var wt = $(window).scrollTop();
+            var wh = $(window).height();
+            var et = $('.howAnimation').offset().top;
+            var eh = $('.howAnimation').outerHeight();
+
+            if (wt + wh >= et && wt + wh - eh * 2 <= et + (wh - eh)) {
+                if (block_show == null || block_show == false && !scrollOff) {
+
+                    animationAll.map(function (index, item) {
+                        setTimeout(() => {
+                            $('.howAnimation .manager ul li').removeClass('active')
+                            $(this).addClass('active')
+                        }, 1000 * index)
+                    })
+
+
+                }
+                block_show = true;
+            } else {
+                if (block_show == null || block_show == true) {
+                    // $('.howAnimation').addClass('active')
+                }
+                block_show = false;
+            }
+        }
+
+        $(window).scroll(function () {
+            if ($(window).width() > 768) {
+                scrollTracking();
+            }
+        });
+    }
+
+
+
+});
+
+$(window).resize(function () {
+    if ($(window).width() < 1220) {
+        $('.tarisfList').slick('refresh');
+        $('.solutionsList').slick('refresh');
+        $('.advList').slick('refresh');
+    }
+
+});
+
